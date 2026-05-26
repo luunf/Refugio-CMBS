9424e4130d1dfa5abeab5e2632fca382a2137cdc
luunf <delialu681@gmail.com>
2026-05-26 20:57:26 -0300

    file firebase acomodado

--- backend/app/firebase_config.py
+++ backend/app/firebase_config.py
@@ -1,6 +1,11 @@
 import firebase_admin
 from firebase_admin import credentials
+import os
 
-cred = credentials.Certificate('firebase-key.json')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+firebase_path = os.path.join(BASE_DIR, 'firebase-key.json')
+
+cred = credentials.Certificate(firebase_path)
 
 firebase_admin.initialize_app(cred)
