275b6cbe6ee6262357a3af936f927a5212d550a0
G10VANN4 <gfmartinez2@alumnos.unsada.edu.ar>
2026-06-19 18:09:59 -0300

    BACK - tareas (notificaciones via mails)
    -1)entren en  https://myaccount.google.com/security  y vean si tiene activada la configuracion en dos pasos
    
    -una vez que verifiquen que este activa, vayan a https://myaccount.google.com/apppasswords
    
    -les va aparecer como un "formulario", en "nombre app" pongan: "Refugio CMBS" y aprieten generar.  Copien el codigo que les aparezca
    
    -ese codigo (sin espacios) es el que va en MAIL_PASSWORD
    
    -en MAIL_USERNAME ponen el mail con el que generaron el codigo

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,8 +9,10 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     FIREBASE_CREDENTIALS_PATH = os.getenv('FIREBASE_CREDENTIALS_PATH', 'firebasekey.json')
-    #MAIL_SERVER = 'smtp.gmail.com'
-    #MAIL_PORT = 587
-    #MAIL_USE_TLS = True
-    #MAIL_USERNAME = 'tu_correo@gmail.com'
-    #MAIL_PASSWORD = 'tu_contraseña_de_aplicacion_de_google'
+
+    MAIL_SERVER = os.getenv('MAIL_SERVER')
+    MAIL_PORT = int(os.getenv('MAIL_PORT', 587))
+    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS', 'True') == 'True'
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.getenv('MAIL_USERNAME')
